c34bbcdcb4660fe61e020f71c916a9099bdf36b5
Ihteshamul Alam <88604856+shimul49@users.noreply.github.com>
2023-11-02 10:29:53 +0600

    Add files via upload

--- src/main/java/io/naztech/security/SecurityApplication.java
+++ src/main/java/io/naztech/security/SecurityApplication.java
@@ -2,9 +2,10 @@ package io.naztech.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
